e9e6be6292c9dfca290d5e335c28acd1cc4eddc7
julianrivera35 <jc.riveram1@uniandes.edu.co>
2023-05-02 03:45:40 -0500

    Algoritmo de fuerza bruta

---
+++ src/Random.java
@@ -0,0 +1,23 @@
+
+
+public class Random {
+
+    public static void main(String[] args) {
+        for(char c1 = 'a'; c1 <= 'z'; c1++){
+            for(char c2 = 'a'; c2 <= 'z'; c2++){
+                for(char c3 = 'a'; c3 <= 'z'; c3++){
+                    for(char c4 = 'a'; c4 <= 'z'; c4++){
+                        for(char c5 = 'a'; c5 <= 'z'; c5++){
+                            for(char c6 = 'a'; c6 <= 'z'; c6++){
+                                for(char c7 = 'a'; c7 <= 'z'; c7++){
+                                    System.out.println("" + c1 + c2 + c3 + c4 + c5 + c6 + c7);
+                                    
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } 
+            }
+    }
+}
